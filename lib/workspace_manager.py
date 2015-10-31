9d9ff395550fc7e05b1852de8033d0e3a3122dd0
David <davideriksson@swedenmail.com>
2015-10-31 21:31:30 +0100

    Fixed small landscape.io smell

--- lib/workspace_manager.py
+++ lib/workspace_manager.py
@@ -8,6 +8,6 @@ class WorkspaceManager(object):
     workspace.project_list.append(project)
     return workspace
 
-  def find_vcs_projects(self, dir):
+  def find_vcs_projects(self, workspace_path):
     workspace2 = Workspace()
     return workspace2

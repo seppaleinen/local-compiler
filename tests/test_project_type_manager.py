640387ab8cfa4b66b34300e859d96b1e593f922b
David <davideriksson@swedenmail.com>
2015-10-31 21:50:08 +0100

    Split up tests to method-level

--- tests/test_project_type_manager.py
+++ tests/test_project_type_manager.py
@@ -4,14 +4,27 @@ import unittest
 from lib.project_type_manager import ProjectTypeManager
 
 
-class doProjectTypeTests(unittest.TestCase):
+class doProjectType_GuessLanguageTests(unittest.TestCase):
   def setUp(self):
     self.manager = ProjectTypeManager()
 
-  def test_guess_language(self):
+  def test_guess_language_somedir(self):
     result = self.manager.guess_language('somedir')
     self.assertEqual(result.value, 'JAVA')
 
-  def test_guess_buildtool(self):
+  def test_guess_language_otherdir(self):
+    result = self.manager.guess_language('otherdir')
+    self.assertEqual(result.value, 'RUBY')
+
+
+class doProjectType_GuessBuildToolTests(unittest.TestCase):
+  def setUp(self):
+    self.manager = ProjectTypeManager()
+
+  def test_guess_buildtool_somedir(self):
     result = self.manager.guess_buildtool('somedir')
     self.assertEqual(result.value, 'MAVEN')
+
+  def test_guess_buildtool_otherdir(self):
+    result = self.manager.guess_buildtool('otherdir')
+    self.assertEqual(result.value, 'BUNDLER')
--- tests/test_server_manager.py
+++ tests/test_server_manager.py
@@ -5,22 +5,53 @@ from lib.server_manager import ServerManager
 from lib.model.enums import ServerDeployStatus, ServerStatus
 
 
-class doServerManagerTests(unittest.TestCase):
+class doServerManager_direct_deploy_to_serverTests(unittest.TestCase):
   def setUp(self):
     self.manager = ServerManager()
 
-  def test_direct_deploy_to_server(self):
+  def test_direct_deploy_to_server_somedir(self):
     result = self.manager.direct_deploy_to_server('somedir')
     self.assertEqual(result, ServerDeployStatus.SUCCESS)
 
-  def test_remote_deploy_to_server(self):
+  def test_direct_deploy_to_server_otherdir(self):
+    result = self.manager.direct_deploy_to_server('otherdir')
+    self.assertEqual(result, ServerDeployStatus.FAILURE)
+
+
+class doServerManager_remote_deploy_to_serverTests(unittest.TestCase):
+  def setUp(self):
+    self.manager = ServerManager()
+
+  def test_remote_deploy_to_server_somedir(self):
     result = self.manager.remote_deploy_to_server('somedir')
     self.assertEqual(result, ServerDeployStatus.SUCCESS)
 
-  def test_server_status(self):
+  def test_remote_deploy_to_server_otherdir(self):
+    result = self.manager.remote_deploy_to_server('otherdir')
+    self.assertEqual(result, ServerDeployStatus.FAILURE)
+
+
+class doServerManager_server_statusTests(unittest.TestCase):
+  def setUp(self):
+    self.manager = ServerManager()
+
+  def test_server_status_somedir(self):
     result = self.manager.server_status('somedir')
     self.assertEqual(result, ServerStatus.ONLINE)
 
-  def test_handle_server(self):
+  def test_server_status_otherdir(self):
+    result = self.manager.server_status('otherdir')
+    self.assertEqual(result, ServerStatus.OFFLINE)
+
+
+class doServerManager_handle_serverTests(unittest.TestCase):
+  def setUp(self):
+    self.manager = ServerManager()
+
+  def test_handle_server_somedir(self):
     result = self.manager.handle_server('somedir')
     self.assertEqual(result, ServerStatus.ONLINE)
+
+  def test_handle_server_otherdir(self):
+    result = self.manager.handle_server('otherdir')
+    self.assertEqual(result, ServerStatus.OFFLINE)

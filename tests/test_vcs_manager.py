c9cf803f038395fcbf78097e76769c6e10ead5fb
David <davideriksson@swedenmail.com>
2015-11-01 12:12:13 +0100

    Separated tests for vcs and workspace manager

--- tests/test_vcs_manager.py
+++ tests/test_vcs_manager.py
@@ -4,13 +4,18 @@ import unittest
 from lib.vcs_manager import VcsManager
 
 
-class doVcsManagerTests(unittest.TestCase):
+class doVcsManager_statusTests(unittest.TestCase):
   def setUp(self):
     self.manager = VcsManager()
 
   def test_status(self):
     result = self.manager.status('somedir')
     self.assertEqual(result, 'somedir')
+
+
+class doVcsManager_updateTests(unittest.TestCase):
+  def setUp(self):
+    self.manager = VcsManager()
 
   def test_update(self):
     result = self.manager.update('somedir')
--- tests/test_workspace_manager.py
+++ tests/test_workspace_manager.py
@@ -5,7 +5,7 @@ from lib.workspace_manager import WorkspaceManager
 from lib.model.models import Workspace, Project
 
 
-class doWorkspaceManagerTests(unittest.TestCase):
+class doWorkspaceManager_add_projectTests(unittest.TestCase):
   def setUp(self):
     self.manager = WorkspaceManager()
 
@@ -16,6 +16,11 @@ class doWorkspaceManagerTests(unittest.TestCase):
 
     self.assertTrue(result.project_list)
     self.assertEqual(result.project_list[0].project_path, 'dir')
+
+
+class doWorkspaceManager_find_vcs_projectsTests(unittest.TestCase):
+  def setUp(self):
+    self.manager = WorkspaceManager()
 
   def test_find_vcs_projects(self):
     # Join basedir of this project to /tests/test_data/workspace_manager

f9a490540a2ce952e58814b2c05219e63dcac8b1
David <davideriksson@swedenmail.com>
2015-11-01 12:36:25 +0100

    Fixed pep8 warning

--- tests/test_workspace_manager.py
+++ tests/test_workspace_manager.py
@@ -3,7 +3,7 @@
 import unittest, os
 from lib.workspace_manager import WorkspaceManager
 from lib.model.models import Workspace
-from liv.model.models import Project
+from lib.model.models import Project
 
 
 class doWorkspaceManager_add_projectTests(unittest.TestCase):

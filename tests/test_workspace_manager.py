b4a9b388b021c9102edc20cf61819e2e3ddbecc4
David <davideriksson@swedenmail.com>
2015-11-01 12:35:00 +0100

    Changed tests to work with git

--- tests/test_workspace_manager.py
+++ tests/test_workspace_manager.py
@@ -24,14 +24,10 @@ class doWorkspaceManager_find_vcs_projectsTests(unittest.TestCase):
 
   def test_find_vcs_projects(self):
     # Join basedir of this project to /tests/test_data/workspace_manager
-    path = os.path.join(os.getcwd(), 'tests', 'test_data', 'workspace_manager')
-    
+    path = os.path.join(os.getcwd())
+
     result = self.manager.find_vcs_projects(path)
 
     self.assertTrue(result.project_list)
 
-    expected_path_1 = os.path.join(path, 'project1')
-    expected_path_2 = os.path.join(path, 'project2')
-
-    self.assertEqual(result.project_list[0].project_path, expected_path_1)
-    self.assertEqual(result.project_list[1].project_path, expected_path_2)
+    self.assertEqual(result.project_list[0].project_path, path)

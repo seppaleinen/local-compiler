387ba66fe4e2fa44c26825409ed6fd63036588b4
David <davideriksson@swedenmail.com>
2015-11-02 18:26:44 +0100

    Added git update and status functionality

--- lib/vcs_manager.py
+++ lib/vcs_manager.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
 
+import subprocess
+import os
+
+def git_command(vcs_path, command):
+  git_dir=os.path.join(vcs_path, '.git')
+
+  git_command = ['git', '--work-tree=' + vcs_path, '--git-dir=' + git_dir, command]
+
+  return subprocess.Popen(git_command, stdout=subprocess.PIPE).communicate()[0]
+
 
 # Class responsible for handling contact with vcs repositories
 class VcsManager(object):
   # Method for checking version control status
   # Takes path of version control and returns result
   def status(self, vcs_path):
-    return vcs_path
+    return git_command(vcs_path, 'status')
 
   # Method for updating version control repository
   # Takes path of directory and returns result
   def update(self, vcs_path):
-    return vcs_path
+    return git_command(vcs_path, 'pull')
--- tests/test_vcs_manager.py
+++ tests/test_vcs_manager.py
@@ -1,22 +1,42 @@
 #!/usr/bin/python
 
 import unittest
+import mock
+import os
 from lib.vcs_manager import VcsManager
 
 
-class doVcsManager_statusTests(unittest.TestCase):
+class VcsManager_status(unittest.TestCase):
   def setUp(self):
     self.manager = VcsManager()
 
-  def test_status(self):
-    result = self.manager.status('somedir')
-    self.assertEqual(result, 'somedir')
+  @mock.patch('lib.vcs_manager.subprocess')
+  def test_status(self, mocked):
+    base_dir = os.getcwd()
+
+    result = self.manager.status(base_dir)
+
+    mocked.Popen.assert_called_with([
+      'git', 
+      '--work-tree=/Users/seppa/Workspace/local-compiler', 
+      '--git-dir=/Users/seppa/Workspace/local-compiler/.git', 
+      'status'], 
+      stdout=mocked.PIPE)
 
 
-class doVcsManager_updateTests(unittest.TestCase):
+class VcsManager_update(unittest.TestCase):
   def setUp(self):
     self.manager = VcsManager()
 
-  def test_update(self):
-    result = self.manager.update('somedir')
-    self.assertEqual(result, 'somedir')
+  @mock.patch('lib.vcs_manager.subprocess')
+  def test_update(self, mocked):
+    base_dir = os.getcwd()
+
+    result = self.manager.update(base_dir)
+
+    mocked.Popen.assert_called_with([
+      'git', 
+      '--work-tree=/Users/seppa/Workspace/local-compiler', 
+      '--git-dir=/Users/seppa/Workspace/local-compiler/.git', 
+      'pull'], 
+      stdout=mocked.PIPE)
